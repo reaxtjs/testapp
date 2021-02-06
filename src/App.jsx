e92f16002e32eeda3ec1508129b28a76ab518e7b
reaxtjs <amin.shayesteh64@gmail.com>
2021-02-06 17:07:21 +0300

    remove junk tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload. Hello how are you
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello React
-        </a>
-      </header>
+    <div>
+      <span>Hello React</span>
     </div>
   );
 }
